e0501b67e82b5fc63d5640f865fa5e33ff8cb890
Mohammad Jafiruzzaman <mohammadjafiruzzaman@gmail.com>
2026-01-11 18:55:26 +0600

    learn about constructors and this keyword in Typescript

---
+++ src/Object Oriented Programming/02 constructor/index.ts
@@ -0,0 +1,25 @@
+namespace Constructors {
+  export class Person {
+    name: string;
+    age: number;
+    religion: string;
+    constructor(name: string, age: number, religion: string) {
+      this.name = name;
+      this.age = age;
+      this.religion = religion;
+    }
+    displayInfo(): void {
+      console.log(
+        `Name: ${this.name} age: ${this.age} and Person's Religion: ${this.religion}`,
+      );
+    }
+  }
+}
+
+let tuhin: Constructors.Person = new Constructors.Person(
+  'Mohammad Tuhin',
+  22,
+  'Islam',
+);
+
+tuhin.displayInfo();
