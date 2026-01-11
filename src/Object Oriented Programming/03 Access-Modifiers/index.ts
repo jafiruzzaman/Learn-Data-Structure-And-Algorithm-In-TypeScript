30aa8ae9c816551c654387c4b295a258216abd97
Mohammad Jafiruzzaman <mohammadjafiruzzaman@gmail.com>
2026-01-11 19:13:52 +0600

    learn about Access-Modifiers in TypeScript

---
+++ src/Object Oriented Programming/03 Access-Modifiers/index.ts
@@ -0,0 +1,31 @@
+namespace AccessModifiers {
+  export class BankAccount {
+    public owner: string; // 🙋‍♂️ Access from Anywhere
+    private balance: number; // 🚨 accessible only inside the class
+    private branch: string; // 🚨 accessible only inside the class
+    constructor(owner: string, balance: number, branch: string) {
+      this.owner = owner;
+      this.balance = balance;
+      this.branch = branch;
+    }
+
+    // set balance using setters
+    public deposit(amount: number): void {
+      this.balance += amount;
+    }
+    public getBalance(): number {
+      return this.balance;
+    }
+  }
+}
+
+// Create a Object of Bank
+const BracBank: AccessModifiers.BankAccount = new AccessModifiers.BankAccount(
+  'Mohammad',
+  500,
+  'Uttara',
+);
+BracBank.deposit(500);
+// BracBank.balance ❌ Error: private
+console.log('Account Owner ', BracBank.owner);
+console.log(BracBank.getBalance());
