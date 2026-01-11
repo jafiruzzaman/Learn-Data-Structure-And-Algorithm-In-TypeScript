c9f0d64331f354600008e12e32683c3ca93c865c
Mohammad Jafiruzzaman <mohammadjafiruzzaman@gmail.com>
2026-01-11 13:05:30 +0600

    variables in typescript

---
+++ src/Typescript/02 variables/index.ts
@@ -0,0 +1,7 @@
+console.log("========================================= variables in typescript =========================================");
+/*
+  What is variable?
+  variable is a container to store values in memory. If you want to store milk you have to store it into a 🍼 bottle
+ */
+let variable = 10;
+console.log(variable);
