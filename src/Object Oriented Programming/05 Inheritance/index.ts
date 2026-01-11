9c784ec31f331be6a27d9261e36163cca34c0234
Mohammad Jafiruzzaman <mohammadjafiruzzaman@gmail.com>
2026-01-11 20:03:42 +0600

    learn about inheritance

---
+++ src/Object Oriented Programming/05 Inheritance/index.ts
@@ -0,0 +1,33 @@
+namespace Inheritance {
+  // this is parent class
+  export class Person {
+    protected name: string;
+    protected age: number;
+    constructor(name: string, age: number) {
+      this.name = name;
+      this.age = age;
+    }
+  }
+  // this is child class
+  export class Employee extends Person {
+    constructor(name: string, age: number, private employeeId: number) {
+      super(name, age);
+    }
+    // setters
+    public setId(employeeId: number): void {
+      this.employeeId = employeeId;
+    }
+    // getters
+    public getId(): void {
+      console.log('Employee Id is: ', this.employeeId);
+    }
+  }
+}
+
+// Create Instance
+let employeeOne: Inheritance.Employee = new Inheritance.Employee(
+  'Mohammad',
+  20,
+  102,
+);
+employeeOne.getId();
