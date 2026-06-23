893278a2d10cac2cb58c87aa3974bbdd6c073601
Mohammad-Jafiruzzaman <mohammadjafiruzzaman@gmail.com>
2026-06-23 23:10:14 +0600

    array: solve left rotate by k places array problem

---
+++ src/01 Array/01 Medium/src/left-rotate-by-k.ts
@@ -0,0 +1,25 @@
+/**
+ * @file left-rotate-by-k.ts
+ * @description left rotate an array by k places
+ */
+
+export {};
+function reverse(arr: number[], start: number, end: number) {
+  while (start < end) {
+    let temp: number = arr[start]!;
+    arr[start] = arr[end]!;
+    arr[end] = temp;
+    (start++, end--);
+  }
+}
+function leftRotateByKPlaces(arr: number[], rotate: number): void {
+  let len = arr.length;
+  rotate = rotate % len;
+  // 0 to rotate-1
+  reverse(arr, 0, rotate - 1);
+  reverse(arr, rotate, len - 1);
+  reverse(arr, 0, len - 1);
+}
+const arr: number[] = [1, 2, 3, 4, 5, 6];
+leftRotateByKPlaces(arr, 3);
+console.log(arr);
