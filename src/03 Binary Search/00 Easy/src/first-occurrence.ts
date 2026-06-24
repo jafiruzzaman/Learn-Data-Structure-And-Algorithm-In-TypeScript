47ad6ad8cc781cae14b1e3d7ad16211af5dafad4
Mohammad-Jafiruzzaman <mohammadjafiruzzaman@gmail.com>
2026-06-24 13:31:10 +0600

    binary-search: solve find the first occurrence of target value in a sorted array

---
+++ src/03 Binary Search/00 Easy/src/first-occurrence.ts
@@ -0,0 +1,27 @@
+/**
+ * @file first-occurrence.ts
+ * @description find the first occurrence of target value in a sorted array
+ */
+
+export {};
+function firstOccurrence(arr: number[], target: number): number {
+  let start = 0,
+    end = arr.length - 1;
+  let res = -1;
+  while (start <= end) {
+    let mid: number = Math.floor(start + (end - start) / 2);
+    if (arr[mid] == target) {
+      res = mid;
+      end = mid - 1;
+    } else if (arr[mid]! < target) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
+  return res;
+}
+
+let arr: number[] = [1, 3, 5, 5, 5, 5, 67, 123, 125],
+  x = 5;
+console.log(firstOccurrence(arr, x));
